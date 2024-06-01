5f5fa403c3e526784ee5be18a57979dfa98d169e
belkhadir <belkhadir.anas@gmail.com>
2024-06-01 23:10:24 +0100

    Add ImageView

---
+++ Source/ImageResourceCore/View/ImageView.swift
@@ -0,0 +1,56 @@
+//
+//  ImageView.swift
+//  ImageResourceCore
+//
+//  Created by Belkhadir Anas on 1/6/2024.
+//
+
+import SwiftUI
+
+struct ImageView<ViewModel: ImageViewDisplayable & ObservableObject>: View {
+    @Environment(\.imageStyle) var imageStyle
+    @ObservedObject private var viewModel: ViewModel
+
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        Group {
+            switch viewModel.loadingState {
+            case .loading, .none:
+                ShimmerView()
+            case .loaded(let result):
+                switch result {
+                case let .success(imageProviding): image(from: imageProviding.data)
+                case .failure: retryLoadImage
+                }
+            }
+        }.onAppear {
+            viewModel.fetchImage()
+        }
+    }
+}
+
+// MARK: - Helpers
+private extension ImageView {
+    func image(from data: Data) -> some View {
+        Image(data: data)
+            .resizable()
+            .frame(width: imageStyle.frame.width, height: imageStyle.frame.height)
+            .scaledToFit()
+            .clipped()
+            .cornerRadius(imageStyle.cornerRadius)
+            .shadow(radius: imageStyle.shadowRadius)
+    }
+    
+    var retryLoadImage: some View {
+        Button(action: {
+            viewModel.fetchImage()
+        }, label: {
+            Image(systemName: "xmark.octagon")
+                .frame(width: 150, height: 200)
+        })
+        .accessibilityIdentifier("RetryButton")
+    }
+}
